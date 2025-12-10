7d67b4c70b7b6ad3d5785b3c05bfe1afc92362e4
Maxi Barmetler <maxi.barmetler@gmail.com>
2025-12-10 18:04:46 +0100

    clang-format

--- d06/main06p2.cpp
+++ d06/main06p2.cpp
@@ -1,7 +1,9 @@
+#include <algorithm>
 #include <coroutine>
 #include <fstream>
 #include <functional>
 #include <iostream>
+#include <optional>
 #include <print>
 #include <ranges>
 #include <vector>
@@ -9,17 +11,39 @@
 
 using namespace std::string_view_literals;
 
+struct operation {
+    char op;
+    uint8_t width;
+};
+
+template<>
+struct std::formatter<operation> : std::formatter<char> {
+    template<class ParseContext>
+    constexpr static ParseContext::iterator parse(ParseContext &ctx) {
+        return ctx.begin();
+    }
+
+    template<class FmtContext>
+    static FmtContext::iterator format(operation s, FmtContext &ctx) {
+        return std::ranges::copy(std::format("{}({})", s.op, s.width), ctx.out()).out;
+    }
+};
 
 int main() {
     std::ifstream f{"../../d06/sample.txt"};
     // std::ifstream f{"../../d06/assignment.txt"};
-    const std::vector<std::string> lines =
-            std::views::iota(0) | std::ranges::views::transform([&f](int) -> std::optional<std::string> {
-                if (std::string line; std::getline(f, line)) return line;
-                return std::nullopt;
-            }) |
-            std::views::take_while([](auto &&opt) { return opt.has_value(); }) |
-            std::views::transform([](auto &&opt) { return opt.value(); }) | std::ranges::to<std::vector>();
-    (void) lines;
+    std::vector<std::string> lines{};
+    for (std::string line; std::getline(f, line);)
+        lines.emplace_back(std::move(line));
+    auto ops = lines.back()
+            | std::views::chunk_by([](auto, auto &&b) { return !"+*"sv.contains(b); })
+            | std::views::transform([](auto &&chunk) {
+                   return operation{.op = chunk[0], .width = static_cast<uint8_t>(chunk.size() - 1)};
+               })
+            | std::ranges::to<std::vector>();
+    const size_t longest_length = std::ranges::max(lines | std::views::transform(&std::string::length));
+    ops.back().width = longest_length - lines.back().length() + 1;
+    lines.pop_back();
+    std::println("{}", ops);
     return 0;
 }
