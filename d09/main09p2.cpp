4025f1dba850587228e080650fc154d5e1454206
Maxi Barmetler <maxi.barmetler@gmail.com>
2025-12-11 18:48:03 +0100

    imgui

--- d09/main09p2.cpp
+++ d09/main09p2.cpp
@@ -1,7 +1,67 @@
+#include <glad/gl.h>
+
+#include <GLFW/glfw3.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
+#include <imgui.h>
+
 #include <fstream>
+#include <iostream>
 
 int main() {
-    std::ifstream f{"../../d09/sample.txt"};
-    // std::ifstream f{"../../d09/assignment.txt"};
+    if (!glfwInit()) return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "ImGui + GLFW + GLAD", nullptr, nullptr);
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    // Load OpenGL (needs GLAD on Windows!)
+    if (gladLoadGL(glfwGetProcAddress) == 0) {
+        std::cerr << "Failed to load OpenGL\n";
+        return -1;
+    }
+
+    // Init ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    // Main Loop
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Hello");
+        ImGui::Text("Cross-platform ImGui + GLFW + GLAD!");
+        ImGui::End();
+
+        ImGui::Render();
+
+        int w, h;
+        glfwGetFramebufferSize(window, &w, &h);
+        glViewport(0, 0, w, h);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
